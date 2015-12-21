56c60724b374be8f7c2fc82e68755b91837bff72
Piotr Popieluch <piotr1212@gmail.com>
2015-12-21 16:28:24 +0100

    build with go 1.5.1, use go and node from epel, use $GOPATH instead of hardcoded path

--- docker/buildcontainer/build.sh
+++ docker/buildcontainer/build.sh
@@ -6,5 +6,5 @@ docker rm gfbuild
 docker build --tag "grafana/buildcontainer" docker/buildcontainer
 
 docker run -i -t \
-  -v /home/torkel/dev/go:/go \
+  -v $GOPATH:/go \
   --name gfbuild grafana/buildcontainer
