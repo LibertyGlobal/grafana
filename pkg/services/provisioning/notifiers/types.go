36c3ea3e11c17bbb268a625b4212a8dcb7082e07
Dmitry Ilyin <idv1985@gmail.com>
2019-03-11 19:25:15 +0100

    LGI: Alert states for notifications
    Decide which notification channels should receive
    which alert states.

--- pkg/services/provisioning/notifiers/types.go
+++ pkg/services/provisioning/notifiers/types.go
@@ -15,16 +15,20 @@ type deleteNotificationConfig struct {
 }
 
 type notificationFromConfig struct {
-	Uid                   string                 `json:"uid" yaml:"uid"`
-	OrgId                 int64                  `json:"org_id" yaml:"org_id"`
-	OrgName               string                 `json:"org_name" yaml:"org_name"`
-	Name                  string                 `json:"name" yaml:"name"`
-	Type                  string                 `json:"type" yaml:"type"`
-	SendReminder          bool                   `json:"send_reminder" yaml:"send_reminder"`
-	DisableResolveMessage bool                   `json:"disable_resolve_message" yaml:"disable_resolve_message"`
-	Frequency             string                 `json:"frequency" yaml:"frequency"`
-	IsDefault             bool                   `json:"is_default" yaml:"is_default"`
-	Settings              map[string]interface{} `json:"settings" yaml:"settings"`
+	Uid                    string                 `json:"uid" yaml:"uid"`
+	OrgId                  int64                  `json:"org_id" yaml:"org_id"`
+	OrgName                string                 `json:"org_name" yaml:"org_name"`
+	Name                   string                 `json:"name" yaml:"name"`
+	Type                   string                 `json:"type" yaml:"type"`
+	SendReminder           bool                   `json:"send_reminder" yaml:"send_reminder"`
+	DisableResolveMessage  bool                   `json:"disable_resolve_message" yaml:"disable_resolve_message"`
+	DisableAlertingMessage bool                   `json:"disable_alerting_message" yaml:"disable_alerting_message"`
+	DisableNoDataMessage   bool                   `json:"disable_no_data_message" yaml:"disable_no_data_message"`
+	DisableUnknownMessage  bool                   `json:"disable_unknown_message" yaml:"disable_unknown_message"`
+	DisablePendingMessage  bool                   `json:"disable_pending_message" yaml:"disable_pending_message"`
+	Frequency              string                 `json:"frequency" yaml:"frequency"`
+	IsDefault              bool                   `json:"is_default" yaml:"is_default"`
+	Settings               map[string]interface{} `json:"settings" yaml:"settings"`
 }
 
 func (notification notificationFromConfig) SettingsToJson() *simplejson.Json {
